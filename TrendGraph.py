604fc3300af5dda81c54ae925f7c93e989e532a7
dratani <dratani@gmail.com>
2019-03-08 09:52:47 -0600

    Add files via upload

---
+++ TrendGraph.py
@@ -0,0 +1,33 @@
+import rrdtool
+import time
+
+ret = rrdtool.graph( "trend2.png",
+                     "--start",'1507573200',
+                     "--end",'15075714200',
+                     "--vertical-label=Carga CPU",
+                     "--title=Uso de CPU",
+                     "--color", "ARROW#009900",
+                     '--vertical-label', "Uso de CPU (%)",
+                     '--lower-limit', '0',
+                     '--upper-limit', '100',
+                     "DEF:carga=trend.rrd:CPUload:AVERAGE",
+                     "AREA:carga#00FF00:CPU load",
+
+
+                     "LINE1:30",
+                     "AREA:5#ff000022:stack",
+                     "VDEF:CPUlast=carga,LAST",
+                     "VDEF:CPUmin=carga,MINIMUM",
+                     "VDEF:CPUavg=carga,AVERAGE",
+                     "VDEF:CPUmax=carga,MAXIMUM",
+
+                    "COMMENT:Now          Min             Avg             Max//n",
+                     "GPRINT:CPUlast:%12.0lf%s",
+                     "GPRINT:CPUmin:%10.0lf%s",
+                     "GPRINT:CPUavg:%13.0lf%s",
+                     "GPRINT:CPUmax:%13.0lf%s",
+                     "VDEF:a=carga,LSLSLOPE",
+                     "VDEF:b=carga,LSLINT",
+                     'CDEF:avg2=carga,a,*,b,+',
+                     "LINE2:avg2#FFBB00" )
+time.sleep(15)
