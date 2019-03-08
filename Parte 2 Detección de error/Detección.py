d84fd8a7ac5f75aae69322fdb77b2bb53049038f
escom <dratani@gmail.com>
2019-03-08 10:04:09 -0600

    :)

---
+++ Parte 2 Detección de error/Detección.py
@@ -0,0 +1,31 @@
+import sys
+import rrdtool
+import time
+ultima_lectura = int(rrdtool.last("trafico1.rrd"))
+tiempo_final = ultima_lectura
+tiempo_inicial = tiempo_final - 3600
+
+
+ret = rrdtool.graphv( "Deteccion.png",
+                     "--start",str(tiempo_inicial),
+                     "--end",str(tiempo_final),
+                     "--vertical-label=Bytes/s",
+                    '--lower-limit', '0',
+                    '--upper-limit', '100',
+                     "DEF:cargaCPU=trend.rrd:CPUload:AVERAGE",
+
+                     "CDEF:umbral25=cargaCPU,25,LT,0,carga,IF",
+                     "VDEF:cargaMAX=carga,MAXIMUM",
+                     "VDEF:cargaMIN=carga,MINIMUM",
+                     "VDEF:cargaSTDEV=carga,STDEV",
+                     "VDEF:cargaLAST=carga,LAST",
+                     "AREA:carga#00FF00:Carga del CPU",
+                     "AREA:carga25#FF9F00:Tráfico de carga mayor que 25",
+                     "HRULE:25#FF0000:Umbral 1 - 25%",
+                     "PRINT:cargaMAX:%6.2lf %SMAX",
+                     "GPRINT:cargaMIN:%6.2lf %SMIN",
+                     "GPRINT:cargaSTDEV:%6.2lf %SSTDEV",
+                     "GPRINT:cargaLAST:%6.2lf %SLAST" )
+print (ret)
+print(ret.keys())
+print(ret.items())
